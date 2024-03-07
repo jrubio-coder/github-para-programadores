f9732db95ebef49ee5b7723db77ce3974f44f43b
jrubio-coder <75922896+jrubio-coder@users.noreply.github.com>
2024-03-07 11:49:23 -0600

    Acabo de agregar otro ejemplo en el arhivo mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,8 @@
 //alert("EDITADO");
 
 console.log("Welcome to TCS Guadalajara JP!")
+
+
+r = 2.3 + 3.5 /**un ejemplo clasico de JS */
+
+console.log(  "El resultado de una suma cualquiera --> " + r)
